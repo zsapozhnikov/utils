fef44e0e9c4321c447dcade68e8a87c5ce4902a5
Evgenii Sapozhnikov <e.sapozhnikhov@xsolla.com>
2024-12-05 17:19:57 +0400

    Change func name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/zsapozhnikov/utils
+module github.com/zsapozhnikov/utils/v2
 
 go 1.23.2
--- utils.go
+++ utils.go
@@ -2,7 +2,7 @@
 
 package utils
 
-func Contains(a []string, x string) bool {
+func InSlice(a []string, x string) bool {
 	for _, n := range a {
 		if x == n {
 			return true

827f95b4cb330f9a6dd5f8d63a5f89d3d3d49ef9
Evgenii Sapozhnikov <e.sapozhnikhov@xsolla.com>
2024-12-05 17:15:36 +0400

    Add new functions ContainsInt

--- utils.go
+++ utils.go
@@ -10,3 +10,12 @@ func Contains(a []string, x string) bool {
 	}
 	return false
 }
+
+func ContainsInt(a []int, x int) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}

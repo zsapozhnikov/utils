eb9f624c696f23ca366157fdabea04aa5f8a356e
Evgenii Sapozhnikov <e.sapozhnikhov@xsolla.com>
2024-12-05 17:30:03 +0400

    Change func name to InSliceInt

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/zsapozhnikov/utils/v2
+module github.com/zsapozhnikov/utils/v3
 
 go 1.23.2
--- utils.go
+++ utils.go
@@ -11,7 +11,7 @@ func InSlice(a []string, x string) bool {
 	return false
 }
 
-func ContainsInt(a []int, x int) bool {
+func InSliceInt(a []int, x int) bool {
 	for _, n := range a {
 		if x == n {
 			return true
